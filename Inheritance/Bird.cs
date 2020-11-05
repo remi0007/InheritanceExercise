53091d9bddbfcf8bd2dcccde0e95c776eca55d43
Remi Douah <remidouah@gmail.com>
2020-11-05 13:29:01 -0600

    Animal Kindom

---
+++ Inheritance/Bird.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inheritance
+{
+    public class Bird : Animal
+
+    {
+        public Bird()
+        {
+            IsAlive = true;
+            LegCount = 2;
+            LandSeeAir = "Air";
+            Age = 0;
+        }
+
+        public string WingColor { get; set; }
+        public bool CanFly { get; set; }
+        public bool WillMigrate { get; set; }
+        public double BeakLength { get; set; }
+    }
+}
--- Inheritance/Program.cs
+++ Inheritance/Program.cs
@@ -8,15 +8,15 @@ namespace Inheritance
         {
             // TODO Be sure to follow best practice when creating your classes
 
-            // Create a class Animal
+            // DONE - Create a class Animal
             // give this class 4 members that all Animals have in common
 
 
-            // Create a class Bird
+            // DONE - Create a class Bird
             // give this class 4 members that are specific to Bird
             // Set this class to inherit from your Animal Class
 
-            // Create a class Reptile
+            // DONE - Create a class Reptile
             // give this class 4 members that are specific to Reptile
             // Set this class to inherit from your Animal Class
 
@@ -29,11 +29,31 @@ namespace Inheritance
              * Creatively display the class member values 
              */
 
+            var myBird = new Bird();
+            myBird.WingColor = "blue";
+            myBird.CanFly = true;
+            myBird.WillMigrate = true;
+            myBird.BeakLength = 3.5;
+
+
             /*Create an object of your Reptile class
              *  give values to your members using the object of your Bird class
              *  
              * Creatively display the class member values 
              */
-        }
+            var Lizard = new Reptile()
+            {
+                IsColdBlooded = true,
+                IsScaly = true,
+                Habitat = "Swamp",
+                CanGrowTail = true,
+
+            };
+
+            Console.WriteLine();
+
+
+
+        }   
     }
 }
